bad29a64e071dbd051bee2d49701bac3dae92c3b
Emmanoel Coutinho <emmanoelcesar967@gmail.com>
2021-05-01 17:23:50 -0300

    first commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <style>
+        body {
+            width: 100vw;
+            height: 100vh;
+            color: whitesmoke;
+        }
+
+        body input {
+            width: 250px;
+            height: 40px;
+
+            outline: none;
+            border-radius: 5px;
+
+            font-size: 16px;
+        }
+    </style>
+
+    <title>Todo App</title>
+</head>
+
+<body class='bg-dark d-flex flex-column justify-content-center align-items-center'>
+    <h1>Adicione uma nova tarefa!</h1>
+    <form method="POST">
+        @csrf
+        <input placeholder="Sua tarefa">
+        <button type="submit" class="btn-success btn">Salvar!</button>
+    </form>
+
+    <!-- Bootstrap -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
